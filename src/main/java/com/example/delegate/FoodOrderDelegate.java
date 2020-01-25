46486ce05a2315d19df195763abc27738bbfd24d
kamalakar <kamalakar@LAPTOP-HK1FK6PA>
2020-01-25 10:50:24 +0530

    find,delete,produce and consume

--- src/main/java/com/example/delegate/FoodOrderDelegate.java
+++ src/main/java/com/example/delegate/FoodOrderDelegate.java
@@ -1,5 +1,13 @@
 package com.example.delegate;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +19,39 @@ public class FoodOrderDelegate {
 	@Autowired
 	private FoodOrderDao dao;
 	
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
+	
 	public void placeOrder(Food food) {
 		System.out.println("placeOrder :: FoodOrderDelegate");
 		dao.save(food);
+	}
+	
+	public Food getOrder(String itemName) {
+		System.out.println("getOrder :: FoodOrderDelegate");
+		Optional<Food> foodData = dao.findById(itemName);
+		return foodData.orElseGet(()->new Food());
+	}
+
+	public List<Food> getMenu() {
+		System.out.println("getMenu :: FoodOrderDelegate");
+		List<Food> foodList = (List<Food>) dao.findAll();
+		return foodList;
+	}
+
+	public void deleteItem(String itemName) {
+		System.out.println("deleteItem :: FoodOrderDelegate");
+		dao.deleteById(itemName);
+	}
+
+	public List<Food> getFoodList(String itemType) {
+		
+		SessionFactory sf = entityManagerFactory.unwrap(SessionFactory.class);
+		Session session = sf.openSession();
+		Query q = session.createQuery("from Food where type=:itemType");
+		q.setParameter("itemType", itemType);
+		System.out.println("query preparation done!!");
+		List<Food> list = q.list();
+		return list;
 	}
 }
--- src/main/java/com/example/services/FoodOrderService.java
+++ src/main/java/com/example/services/FoodOrderService.java
@@ -1,10 +1,16 @@
 package com.example.services;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.delegate.FoodOrderDelegate;
@@ -15,26 +21,49 @@ public class FoodOrderService {
 
 	@Autowired
 	private FoodOrderDelegate delegate;
-	
-	@RequestMapping(value="/placeOrder",consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.POST)
+
+	@RequestMapping(value = "/placeOrder", consumes = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.POST)
 	public String placeOrder(@RequestBody Food food) {
 		System.out.println(food.getItemName());
 		System.out.println(food.getPrice());
 		delegate.placeOrder(food);
 		return "succesfully placed order";
 	}
-	
-	@RequestMapping(value = "/getOrder", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-	public Food getOrder() {
+
+	@RequestMapping(value = "/getOrder/{item}", produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
+	public Food getOrder(@PathVariable("item") String itemName) {
 		System.out.println("getOrder::FoodOrderServices");
-		Food food = new Food();
-		food.setItemName("Mutton Biryani");
-		food.setPrice(450);
-		food.setQuantity("Full");
-		food.setType("Non-Veg");
-		
+		System.out.println("Item requested for : " + itemName);
+		Food food = delegate.getOrder(itemName);
+
 		return food;
 	}
+
+	@RequestMapping(value = "/getMenu", produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
+	public List<Food> getMenu() {
+		System.out.println("getMenu::FoodOrderServices");
+		List<Food> foodList = delegate.getMenu();
+		return foodList;
+	}
+
+	@RequestMapping(value="/deleteItem/{item}", method = RequestMethod.DELETE)
+	public String deleteItem(@PathVariable("item") String itemName) {
+		System.out.println("deleteItem :: FoodOrderServices");
+		delegate.deleteItem(itemName);
+		return "successfully deleted " + itemName;
+	}
+	
+	@RequestMapping(value="/getFoodList/{type}", produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
+	public List<Food> getFoodListByType(@PathVariable("type") String itemType) {
+		System.out.println("getFoodsByType :: FoodOrderServices");
+		List<Food> foodList = delegate.getFoodList(itemType);
+		return foodList;
+	}
+
 	/*
 	 * @RequestMapping(value="/placeOrderXml", consumes =
 	 * MediaType.APPLICATION_XML_VALUE, method = RequestMethod.POST) public String
